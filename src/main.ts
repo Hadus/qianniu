45978b7e9e28740276ccda000ad20feac60ea995
kerian <18700872228@163.com>
2023-08-28 00:33:07 +0800

    add

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,18 @@ import './assets/css/icon.css';
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
-
 // 注册elementplus图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component);
+  app.component(key, component);
 }
 // 自定义权限指令
 const permiss = usePermissStore();
 app.directive('permiss', {
-    mounted(el, binding) {
-        if (!permiss.key.includes(String(binding.value))) {
-            el['hidden'] = true;
-        }
-    },
+  mounted(el, binding) {
+    if (!permiss.key.includes(String(binding.value))) {
+      el['hidden'] = true;
+    }
+  },
 });
 
 app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
 export default defineConfig({
 	base: './',
 	plugins: [
@@ -18,5 +19,9 @@ export default defineConfig({
 	],
 	optimizeDeps: {
 		include: ['schart.js']
-	}
+	},
+	server: {
+		port: 3000,
+		open: true,
+	},
 });
