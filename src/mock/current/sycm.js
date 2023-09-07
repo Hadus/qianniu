0696a8e3b5c341e1135ee8ed4d4553f22d1d40b4
kerian <18700872228@163.com>
2023-09-07 23:57:15 +0800

    update: get data

---
+++ src/mock/current/sycm.js
@@ -0,0 +1,21 @@
+// 用户
+const user = {
+
+};
+// 首页
+const home = {};
+
+// 店铺
+const shop = {};
+
+// 交易
+const trade = {};
+
+// 物流
+const wuliu = {};
+
+// 数据(生意参谋)
+const sycm = {};
+
+// 数据大屏
+const dataScreen = {};
