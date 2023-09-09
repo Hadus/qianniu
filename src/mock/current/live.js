64258acc53bd8a2454150d205f0cca78f799820b
kerian <18700872228@163.com>
2023-09-10 00:25:28 +0800

    add: live data

---
+++ src/mock/current/live.js
@@ -0,0 +1,10 @@
+// 直播页
+const  live_table_obj = require('@/mock/current/tableData/live_list.json');
+
+const live_table_list={
+  table_1: live_table_obj.table_1_2.table_1,
+  table_2: live_table_obj.table_1_2.table_2,
+  table_3: live_table_obj.table_3_4.table_3,
+  table_4: live_table_obj.table_3_4.table_4,
+};
+export default live_table_list;
