9281ae02a59a39d46b5ac149100a4c5fd4c22a87
kerian <18700872228@163.com>
2023-09-10 22:12:33 +0800

    update: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -89,13 +89,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   document.title = `${to.meta.title}`;
-  const role = localStorage.getItem('ms_username');
-  if (!role && to.path !== '/login') {
-    next('/login');
-  } else {
-    next();
-  }
-
+  next();
 });
 
 export default router;
