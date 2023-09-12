25a3d2fe48f5d6af987f4bb1a7335aaa79b249fd
kerian <18700872228@163.com>
2023-09-12 17:13:39 +0800

    update: mock

--- src/mock/current/user.js
+++ src/mock/current/user.js
@@ -3,6 +3,6 @@ const user = {
   name:'贝莎珠宝', // 店铺名称
   avatarImg: 'src/mock/current/img/shop.jpg', // 头像照片
   taobaoName: 'tb154847218995', // 淘宝账号名
-  xinyonglevel: 2
+  xinyonglevel: 2 // 信用等级
 };
 export default user;
